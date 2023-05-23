4b49b88dcdf283e6b1a0050d497e1331c722d5db
seokho7799 <seokho7799@gmail.com>
2023-05-23 11:55:31 +0900

    add post entity

---
+++ app/src/main/java/com/example/lawjoin/data/model/Post.kt
@@ -0,0 +1,15 @@
+package com.example.lawjoin.data.model
+
+import java.util.Date
+
+class Post(
+    val id: Int,
+    val title: String,
+    val detail: String,
+    val owner: String,
+    val createTime: Date,
+    val modifyTime: Date,
+    val isAnonymous: Boolean,
+    val isForOnlyLawyer: Boolean,
+    val recommendationCount: Int
+)
