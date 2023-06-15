d58a366781ca4a1bcb8b37b5a0e80d2f8db552d6
lunev <who1061@naver.com>
2023-06-15 17:07:27 +0900

    한상

--- app/src/main/java/com/example/lawjoin/account/MenuAdapter.kt
+++ app/src/main/java/com/example/lawjoin/account/MenuAdapter.kt
@@ -14,6 +14,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.lawjoin.R
 import com.example.lawjoin.data.objects.MenuObjects
 import com.example.lawjoin.lawyerdetail.LawyerDetailActivity
+import com.example.lawjoin.post.MyBoardFreeActivity
+import com.example.lawjoin.word.MyLikeLawWordActivity
 import com.kakao.sdk.user.UserApiClient
 
 @RequiresApi(Build.VERSION_CODES.O)
@@ -35,23 +37,23 @@ class MenuAdapter(private val menuSet: ArrayList<MenuObjects>, var context:Conte
                         putExtra("data", userId)
                     }.run{context.startActivity(this)}
 
-                    //"북마크한 법률 단어 보기" -> Intent(context, ::class.java).apply{
-                    //    putExtra("data", userId)
-                    //}.run{context.startActivity(this)}
+                    "북마크한 법률 단어 보기" -> Intent(context, MyLikeLawWordActivity::class.java).apply{
+                        putExtra("data", userId)
+                    }.run{context.startActivity(this)}
 
-                    //"내가 쓴 글" -> Intent(context, LawyerProfileDetailActivity::class.java).apply{
-                    //    putExtra("data", userId)
-                    //}.run{context.startActivity(this)}
+                    "내가 쓴 글" -> Intent(context, MyBoardFreeActivity::class.java).apply{
+                        putExtra("data", userId)
+                    }.run{context.startActivity(this)}
 
-                    //"내 정보" -> Intent(context, LawyerProfileDetailActivity::class.java).apply{
-                    //    putExtra("data", userId)
-                    //}.run{context.startActivity(this)}
+                    /*"내 정보" -> Intent(context, LawyerProfileDetailActivity::class.java).apply{
+                        putExtra("data", userId)
+                    }.run{context.startActivity(this)}
 
-                    //"앱 설정" -> Intent(context, LawyerProfileDetailActivity::class.java).apply{
-                    //    putExtra("data", userId)
-                    //}.run{context.startActivity(this)}
+                    "앱 설정" -> Intent(context, LawyerProfileDetailActivity::class.java).apply{
+                        putExtra("data", userId)
+                    }.run{context.startActivity(this)}
 
-                    //"로그아웃" -> 로그아웃 하는 코드로 팝업창? 띄우고 로그아웃 하는 코드 작성
+                    "로그아웃" -> 로그아웃 하는 코드로 팝업창? 띄우고 로그아웃 하는 코드 작성
                     "로그아웃" -> UserApiClient.instance.logout { error ->
                         if (error != null) {
                             Log.e("log", "로그아웃 실패. SDK에서 토큰 삭제됨", error)
@@ -59,8 +61,7 @@ class MenuAdapter(private val menuSet: ArrayList<MenuObjects>, var context:Conte
                         else {
                             Log.i("log", "로그아웃 성공. SDK에서 토큰 삭제됨")
                         }
-                    }
-
+                    } */
                 }
             }
         }
